531814bafb5158493e8f5daef116263ffab955fb
Harman <hrrsandhu6@gmail.com>
2021-04-28 16:30:15 -0700

    Prod App.js

--- app/App.js
+++ app/App.js
@@ -1,161 +1,78 @@
-import React, { useState } from 'react';
-import { Button, Text, View, SafeAreaView, ActivityIndicator, StyleSheet } from 'react-native';
-import { ApolloProvider, useQuery, gql } from '@apollo/client';
-import { Picker } from '@react-native-picker/picker';
+import React, {useState} from 'react'
+import {View, Text, Button} from 'react-native'
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
 
-import { apolloClient } from './apollo';
 
-// Imperial I-class Star Destroyer
-const defaultStarshipId = 'c3RhcnNoaXBzOjM=';
-
-const LIST_STARSHIPTS = gql`
-  query listStarships {
-    allStarships {
-      starships {
-        id
-        name
-      }
-    }
-  }
-`;
-
-const GET_STARSHIP = gql`
-  query getStarship($id: ID!) {
-    starship(id: $id) {
-      id
-      name
-      model
-      starshipClass
-      manufacturers
-      length
-      crew
-      costInCredits
-      consumables
-      filmConnection {
-        films {
-          id
-          title
-        }
-      }
-    }
-  }
-`;
-
-function RootComponent() {
-  const [starshipId, setStarshipId] = useState(defaultStarshipId);
-  const { data, error, loading } = useQuery(GET_STARSHIP, {
-    variables: { id: starshipId },
-  });
-
-  if (error) { console.log('Error fetching starship', error); }
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.section}>
-        <StarshipPicker
-          starshipId={starshipId}
-          onStarshipChange={setStarshipId}
-        />
-      </View>
-      {loading ? (
-        <ActivityIndicator color='#333' />
-      ) : (
-        <StarshipDetails starship={data.starship} />
-      )}
-    </View>
-  );
+// this is the demo main screen
+// will be moved to a different file
+function MainScreen({navigation}){
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Main screen</Text>
+            <Button 
+                title="Sign Up" 
+                onPress={() => navigation.navigate('SignUp')}/>
+        </View>
+    );
 }
 
-function StarshipPicker(props) {
-  const { data, error, loading } = useQuery(LIST_STARSHIPTS);
-
-  if (error) { console.log('Error listing starships', error) }
-  if (loading) return null;
-
-  const { starships } = data.allStarships;
-
-  return (
-    <Picker
-      selectedValue={props.starshipId}
-      onValueChange={props.onStarshipChange}
-    >
-      {starships.map(starship => (
-        <Picker.Item key={starship.id} label={starship.name} value={starship.id} />
-      ))}
-    </Picker>
-  )
+// this is the demo signup screen
+// will be moved to a different file
+function SignUpScreen({navigation}){
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Sign Up bro</Text>
+            <Button 
+                title="Go back" 
+                onPress={() => navigation.goBack()} />
+        </View>
+    );
 }
 
-function StarshipDetails({ starship }) {
-  return (
-    <>
-      <View style={styles.section}>
-        <Text style={styles.starshipName}>{starship.name}</Text>
-        <Text style={styles.starshipModel}>{starship.model}</Text>
-      </View>
 
-      <View style={styles.section}>
-        <Text style={styles.label}>AAAAAAAAALOOOOOOOOOOOOO</Text>
-        <Text>- {starship.crew} crew members</Text>
-        <Text>- {starship.consumables} without restocking</Text>
-      </View>
+// here
+const Stack = createStackNavigator()
 
-      <View>
-        <Text style={styles.label}>Ship attributes</Text>
-        <Text>- {starship.length}m long</Text>
-        <Text>- {starship.costInCredits} credits</Text>
-      </View>
-
-      <View style={styles.section}>
-        <Text style={styles.label}>Manufacturers</Text>
-        {starship.manufacturers.map(manufacturer => (
-          <Text key={manufacturer}>- {manufacturer}</Text>
-        ))}
-      </View>
-
-      <View style={styles.section}>
-        <Text style={styles.label}>Appeared in</Text>
-        {starship.filmConnection.films.map(film => (
-          <Text key={film.id}>- {film.title}</Text>
-        ))}
-      </View>
-    </>
-  )
+// App component always stays here
+export default function App(){
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                {/*Main Screen stack*/}
+                <Stack.Screen 
+                    name="Main" 
+                    component={MainScreen}
+                    options={{ 
+                            title: '',
+                            headerTransparent: true
+                            }}
+                />
+                {/*Sign Up Screen stack*/}
+                 <Stack.Screen 
+                    name="SignUp" 
+                    component={SignUpScreen}
+                    options={{ 
+                            title: 'Sign Up',
+                            headerTransparent: true
+                            }}
+                />
+                                  
+              
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-const styles = StyleSheet.create({
-  loadingContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 50,
-  },
-  label: {
-    marginBottom: 2,
-    fontSize: 12,
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-  },
-  section: {
-    marginVertical: 12,
-  },
-  starshipName: {
-    fontSize: 32,
-    fontWeight: 'bold',
-  },
-  starshipModel: {
-    fontStyle: 'italic',
-  },
-});
 
-export default function App() {
-  return (
-    <ApolloProvider client={apolloClient}>
-      <RootComponent />
-    </ApolloProvider>
-  );
-}
+
+
+
+
+/**
+ * 
+ * Notes:
+ * - Conventions:
+ * * - capitalize route names
+ * * screen names should be - <Name>Screen
+ */
