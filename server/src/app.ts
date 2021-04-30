9f3df3d0c9b420026bef3d9aa39e0b6a53fe66dc
Harman <hrrsandhu6@gmail.com>
2021-04-29 18:05:19 -0700

    Create app.ts

---
+++ server/src/app.ts
@@ -0,0 +1,40 @@
+import express from 'express'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+import { ApolloServer } from 'apollo-server-express'
+import { createServer } from "http"
+import cors from "cors"
+// import schema from "./data/schema"
+import { btw_api_schema } from './data/schemax'
+
+const app:express.Application = express()
+const PORT:number = 3030
+
+
+app.use(morgan("tiny"))
+app.use(cors())
+app.use(bodyParser.json({ limit: '5mb' }))
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: false }))
+app.set('json spaces', 2);
+
+const server = new ApolloServer({
+    schema: btw_api_schema,
+    subscriptions: { path: "/websockets" },
+    context: ({ req }) => ({ req })
+})
+
+
+app.get("/custom", (req:any, res:any) =>{
+    res.json({message: "hello"})
+})
+
+
+server.applyMiddleware({app})
+const httpServer = createServer(app)
+server.installSubscriptionHandlers(httpServer)
+
+
+httpServer.listen(PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`);
+})
