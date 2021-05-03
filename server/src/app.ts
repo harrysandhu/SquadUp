2986253f6826245eb149f57419822a49afbe98f8
Harman <hrrsandhu6@gmail.com>
2021-05-03 01:13:49 -0700

    x-api-key authorization

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,17 @@
 import express from 'express'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
-import { ApolloServer } from 'apollo-server-express'
+import { ApolloServer, ForbiddenError } from 'apollo-server-express'
 import { createServer } from "http"
 import cors from "cors"
 // import schema from "./data/schema"
 // import { btw_api_schema } from './data/schemax'
 import { squadup_schema_v1 } from './schemas/schema';
+import {PrismaClient} from '@prisma/client'
 
+const prisma = new PrismaClient()
 
+const validClient = "squadup69"
 
 const app:express.Application = express()
 const PORT:number = 3030
@@ -20,10 +23,18 @@ app.use(bodyParser.json({ limit: '5mb' }))
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: false }))
 app.set('json spaces', 2);
 
+
+
 const server = new ApolloServer({
     schema: squadup_schema_v1,
     subscriptions: { path: "/websockets" },
-    context: ({ req }) => ({ req })
+    context: async ({ req }) => {
+        let apitoken = req.headers["x-api-key"]
+        if(validClient != apitoken){
+            throw new ForbiddenError("This client is unauthorized.")
+        }
+        return {req}
+    }
 })
 
 
