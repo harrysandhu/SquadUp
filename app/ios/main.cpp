3f5a2e5a2afc675945ee23b29e03972840ed6f35
gandhis88 <56126906+gandhis88@users.noreply.github.com>
2021-05-04 12:33:10 -0700

    computer fixed

---
+++ app/ios/main.cpp
@@ -0,0 +1,8 @@
+//
+//  main.cpp
+//  app
+//
+//  Created by Simran  Gandhi on 2021-05-03.
+//
+
+#include <stdio.h>
---
+++ app/ios/main.m
@@ -0,0 +1,8 @@
+//
+//  main.m
+//  app
+//
+//  Created by Simran  Gandhi on 2021-05-03.
+//
+
+#import <Foundation/Foundation.h>
