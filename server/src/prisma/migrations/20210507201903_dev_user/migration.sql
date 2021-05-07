c99e02d82f5fe1866c51284d96cab6917e7d1a38
Harman <hrrsandhu6@gmail.com>
2021-05-07 13:27:50 -0700

    schema update - remove dID unique constraint

--- server/src/prisma/migrations/20210507201903_dev_user/migration.sql
+++ server/src/prisma/migrations/20210507201903_dev_user/migration.sql
@@ -3,24 +3,6 @@ CREATE TABLE `Device` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `deviceId` VARCHAR(100) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-UNIQUE INDEX `Device.deviceId_unique`(`deviceId`),
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `Auth` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `uid` VARCHAR(191) NOT NULL,
-    `userId` VARCHAR(100),
-    `idToken` VARCHAR(1024),
-    `email` VARCHAR(256) NOT NULL,
-    `password` VARCHAR(100),
-    `authStage` ENUM('SIGNUP', 'USERNAME', 'SUB', 'FINAL') NOT NULL DEFAULT 'SIGNUP',
-    `authType` ENUM('GOOGLE', 'FACEBOOK', 'EMAIL') NOT NULL DEFAULT 'GOOGLE',
-UNIQUE INDEX `Auth.userId_unique`(`userId`),
-UNIQUE INDEX `Auth.email_unique`(`email`),
-UNIQUE INDEX `Auth_uid_unique`(`uid`),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -28,13 +10,37 @@ UNIQUE INDEX `Auth_uid_unique`(`uid`),
 -- CreateTable
 CREATE TABLE `User` (
     `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(100),
+    `idToken` VARCHAR(1024),
+    `email` VARCHAR(256) NOT NULL,
+    `password` VARCHAR(100),
+    `authStage` ENUM('SIGNUP', 'USERNAME', 'SUB', 'FINAL') NOT NULL DEFAULT 'SIGNUP',
+    `authType` ENUM('GOOGLE', 'FACEBOOK', 'EMAIL') NOT NULL DEFAULT 'GOOGLE',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `dob` DATETIME(3) NOT NULL,
+    `dID` INTEGER NOT NULL,
+UNIQUE INDEX `User.userId_unique`(`userId`),
+UNIQUE INDEX `User.email_unique`(`email`),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Profile` (
+    `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(100),
     `username` VARCHAR(100),
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-UNIQUE INDEX `User.username_unique`(`username`),
+    `avatarUrl` VARCHAR(1024) NOT NULL DEFAULT 'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/default-avatar.png',
+    `bio` VARCHAR(200),
+    `uID` VARCHAR(191) NOT NULL,
+UNIQUE INDEX `Profile.username_unique`(`username`),
+UNIQUE INDEX `Profile_uID_unique`(`uID`),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Auth` ADD FOREIGN KEY (`uid`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `User` ADD FOREIGN KEY (`dID`) REFERENCES `Device`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Profile` ADD FOREIGN KEY (`uID`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
