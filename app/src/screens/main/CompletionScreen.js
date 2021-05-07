c014a4a9e0ec1606ebd11da722a61b955691927a
Harman <hrrsandhu6@gmail.com>
2021-05-06 22:19:38 -0700

    Resolved dependency issues

---
+++ app/src/screens/main/CompletionScreen.js
@@ -0,0 +1,214 @@
+// TODO : Clean up - fix indentation
+import React, {useState, useEffect} from 'react'
+import { View, Text, Button, Image, KeyboardAvoidingView, Platform, Keyboard } from 'react-native'
+import { TopText, FlexView, InputTF, VFlex, HFlex, AtLabel, 
+         BackArrow, InputDOB, ButtonPrimary, ButtonView, 
+         ImageSelectorTouchable, TopTitle, TopBar } from '../../components/styled/components';
+import Icon from 'react-native-vector-icons/FontAwesome'
+import * as ImagePicker from 'expo-image-picker';
+import { Input as InputEl } from "react-native-elements";
+import { Header, Icon as Icc } from "react-native-elements";
+import * as SecureStore from 'expo-secure-store'
+import {useQuery, useMutation, gql, useApolloClient } from "@apollo/client"
+import DeviceInfo from 'react-native-device-info';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+
+
+
+
+const GET_PROFILE = gql `
+    query getProfile($username: String!){
+        profile(username: $username){
+            username
+        }
+    }
+`
+
+const SET_USERNAME = gql`
+    mutation setUsername($data: SetUsername!){
+        setUsername(data: $data){
+            username
+        }
+    }
+`
+
+export function CompletionScreen({route, navigation}){
+    const client = useApolloClient();
+    const [usernameText, setUsernameText]  = useState("")
+    const [usernameIsActive, setUsernameIsActive] = useState(false)
+
+    const [DOB, setDOB] = useState("")
+    const [DOBIsActive, setDOBIsActive] = useState(false)
+    const [user, setUser] = useState(route.params.user)
+    let imgUrl = "https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/default-avatar.png"
+    const [image, setImage] = useState((user.profile.avatarUrl) ? user.profile.avatarUrl : imgUrl );
+    const [usernameState, setUsernameMessage ] = useState({message: "", state: false})
+    const loadUser = async () => {
+        return await SecureStore.getItemAsync("user")
+    }
+    // const [user, setUser] = useState(loadUser())
+ 
+    
+    useEffect(() => {
+
+        (async () => {
+          if (Platform.OS !== 'web') {
+            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            if (status !== 'granted') {
+              alert('Sorry, we need camera roll permissions to make this work!');
+            }
+          }
+        })();
+      }, []);
+
+    useEffect(()=>{
+        navigation.addListener('beforeRemove', (e) => {
+            e.preventDefault()
+            console.log("goingback")
+        })
+    }, [navigation])
+
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            base64: true,
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+    
+    
+        if (!result.cancelled) {
+          setImage(result.uri);
+          console.log(result.base64);
+        }
+        
+        console.log("Result:::", result)
+
+    };
+
+    async function handleContinue(){
+        console.log(user.profile.id)
+        try{
+            let data = {
+                username: usernameText,
+                profile_id: user.profile.id
+            }
+            let result = await client.mutate({
+                mutation: SET_USERNAME, 
+                variables: {
+                    data: data
+                }
+            })
+            if(result.data.setUsername.username != null){
+                user.profile.username = result.data.setUsername.username
+                setUser(user)
+            }
+            alert("success")
+            navigation.navigate('Home', {user: user})
+        }catch(e){
+            alert("Something went wrong")
+        }
+    }
+
+    async function handleUsernameValueChange(value) {
+        try{
+            let result = await client.query({
+                query: GET_PROFILE,
+                variables: {username: value},
+                fetchPolicy: "network-only"
+            })
+            
+            if(result.data.profile == null){
+                setUsernameMessage({message: "Username is available", state: true})
+            }else{
+                setUsernameMessage({message: "Username not available", state: false})
+            }
+
+        }
+       catch(e){
+           alert("Nework Error")
+       }
+    }
+
+    // imageData(base64)  --->  firebase save (base64) : returns -> url -----> console.log(url) END
+    // https://firebase-cdn.com/appid/assets/abc123/images/avatar.jpg
+
+    return (
+        <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#070A1E'}}
+        >
+        
+        <TopBar state={usernameState.state} message={usernameState.message}>
+            <Text style={{color:"white"}}>{usernameState.message}</Text>
+        </TopBar>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#070A1E'}}>
+        
+             <Header
+            barStyle="default"
+            centerComponent={
+                <VFlex>
+                <TopTitle>COMPLETE YOUR PROFILE</TopTitle>
+                </VFlex>
+                }
+            centerContainerStyle={{}}
+                containerStyle={{ width: '100%', 
+                backgroundColor: 'none', 
+                position:'absolute', 
+                top:-40, 
+                borderBottomColor: "transparent"
+            }}
+            leftComponent={   
+                <></>
+                }
+            placement="center"
+            rightComponent={{}}
+            rightContainerStyle={{}}
+            
+            statusBarProps={{}}
+    />
+            <ImageSelectorTouchable title="Choose Image from Camera Roll" onPress={pickImage} >
+                
+                {image && <Image source={{ uri: image }} style={{width: 200, height: 200 }} />}
+            </ImageSelectorTouchable>
+
+            <FlexView style={{paddingTop: "3%"}}>
+                <HFlex>
+                <Icon name='user' size={25} style={{color:"#a9a9a9", paddingTop: 20, paddingRight: 20}}/>
+                 <InputTF
+                        placeholder="Username"
+                        placeholderTextColor="#a9a9a9"
+                        active={usernameIsActive} 
+                        value={usernameText}
+                        autocorrect="false"
+                        onFocus={()=>{
+                            setUsernameIsActive(true)
+                        }}
+
+                        onBlur={() =>{
+                            setUsernameIsActive(false)
+                        }}
+                            onChangeText={async (value) =>{
+                            setUsernameText(value)
+                            await handleUsernameValueChange(value)
+                        }}
+
+                    /> 
+                </HFlex>
+            </FlexView>
+
+            <ButtonView>
+                <ButtonPrimary style={{bottom: "-15%"}} state={usernameState.state} onPress={async () => await handleContinue()}>
+                    <Text style={{color:"white"}}>CONTINUE</Text>
+                </ButtonPrimary>
+            </ButtonView>
+
+        </View>
+    
+          </KeyboardAvoidingView>
+    );
+}
+
+
---
+++ app/src/screens/main/SignUpScreen.js
@@ -0,0 +1,245 @@
+// TODO : Clean up - fix indentation
+
+// React UI Components
+import React, {useState, useEffect} from 'react'
+import {View, Text, Button, Image} from 'react-native'
+import { TopText, BackArrow, HFlex } from '../../components/styled/components';
+import { ButtonPrimary, 
+    ButtonSecondary, 
+    ButtonView, 
+    VFlex, 
+    LogoImage, 
+    TopTitle, 
+    ButtonGoogle, 
+    ButtonFacebook} from  '../../components/styled/components';
+    import Icon from 'react-native-vector-icons/FontAwesome'
+import { Header, Icon as Icc } from "react-native-elements";
+
+
+// Utils
+import * as SecureStore from 'expo-secure-store'
+import DeviceInfo from 'react-native-device-info';
+
+
+// Firebase
+import { firebaseConfig } from '../../../firebase.config';
+import auth from '@react-native-firebase/auth';
+import {
+    GoogleSignin,
+    GoogleSigninButton
+} from '@react-native-google-signin/google-signin'; 
+GoogleSignin.configure(firebaseConfig);
+
+// Apollo
+import {useQuery, useMutation, gql, useApolloClient } from "@apollo/client"
+import { GET_DEVICE, GET_USER_BY_EMAIL } from '../../apollo/queries';
+import { SIGNUP_USER } from '../../apollo/mutations';
+
+
+
+
+// facebook app id : 1676299799220590
+
+export function SignUpScreen({navigation}) {
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
+    const [userInput, setUserInput] = useState({email: "yoosffs@dga.com"});
+    const [email, setEmail] = useState("yoosffs@dga.com")
+    const [deviceId, setDeviceId] = useState(DeviceInfo.getUniqueId())
+    const [buttonState, setButtonState] = useState(false)
+    const client = useApolloClient();
+
+    /*
+    * @Query
+    * GET DEVICE'S ID BY deviceId
+    */ 
+    const getDevice = useQuery(GET_DEVICE, {
+            variables : {
+                deviceId: deviceId
+            }
+        })
+
+        // if(!getDeviceData) return null
+    /*
+    * @Mutation
+    * Sign Up user
+    */ 
+
+    const [signUpUser, {mutData}] = useMutation(SIGNUP_USER)
+
+
+    const Provider = Object.freeze({
+        'GOOGLE': 1,
+        'FACEBOOK': 2
+    })
+    const [provider, setProvider] = useState(Provider.GOOGLE)
+    
+
+    // Handle user state changes
+    async function onAuthStateChanged(user) {
+       
+
+    
+            console.log("Device Data", getDevice.data)
+            console.log("USER::::", user)
+            
+            let userInput = {}
+            if(user != null){
+                if (provider == Provider.GOOGLE){
+                    userInput.name = user.displayName
+                    userInput.email = user.email
+                    userInput.avatarUrl = user.photoURL
+                    userInput.userId = user.uid
+                    userInput.dID = getDevice.data.device.id
+                }
+                
+                setUserInput(userInput)
+                await SecureStore.setItemAsync("userInput", JSON.stringify(userInput))
+            }
+          
+            if(user != null){
+                try{
+                    const getUser = await client.query({
+                        query: GET_USER_BY_EMAIL,
+                        variables: {email: userInput.email},
+                        fetchPolicy: "network-only"
+                    })
+                    console.log("WE GOT HIM", getUser.data)
+                    console.log(userInput)
+                    if(getUser.data.userByEmail){
+                        console.log("user exists, signing in")
+                        if(getUser.data.userByEmail.profile.username == null){
+                            // username selection
+                            console.log("navigating to completion")
+                            await SecureStore.setItemAsync("user", JSON.stringify(getUser.data))
+                            navigation.navigate('Completion', {user: getUser.data.userByEmail})
+                        }else{
+                            // home
+                            navigation.navigate('Home', {user: getUser.data.userByEmail})
+                            console.log("ethe")
+                        }
+
+                    }else{
+                        console.log("user doesnt exist, signing up")
+                        console.log(userInput)
+                        const signUpUser = await client.mutate({
+                            mutation: SIGNUP_USER,
+                            variables: {userInput: userInput}
+                        })
+                        if(signUpUser.data.id){
+                            console.log("navigating to completion")
+                            await SecureStore.setItemAsync("user", JSON.stringify(signUpUser.data))
+                            navigation.navigate('Completion', {user: signUpUser.data.user})
+                        }
+                        
+                        console.log(signUpUser)
+                     }
+                }catch(e){
+                    // sign up user
+                    // network error
+                    console.log(e.message)
+                    alert("Something went wrong!")
+                }
+               
+            }
+
+            setUser(user);
+        if (initializing) setInitializing(false);
+        
+    }
+  
+    async function onFacebookButtonPress(){
+        setProvider(Provider.FACEBOOK)
+        // TODO Facebook login
+    }
+
+    async function onGoogleButtonPress() {
+        setButtonState(true)
+        setProvider(Provider.GOOGLE)
+        // Get the users ID token
+        const { idToken } = await GoogleSignin.signIn();
+      
+        // Create a Google credential with the token
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      
+        // Sign-in the user with the credential
+        return auth().signInWithCredential(googleCredential);
+      }
+    
+    
+    async function saveAuthState(){
+        console.log(buttonState)
+        if(buttonState){
+
+            console.log("THIS RAN")
+            await SecureStore.setItemAsync("AUTH_STATE", "SIGNUP")
+            console.log(userInput)
+        
+        }
+    
+      }
+
+
+  useEffect(() => {
+        // auth().signOut().then(() => {
+        //     console.log("signed out yo")
+        // })
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+           
+
+       
+        return subscriber; // unsubscribe on unmount
+    
+    
+  }, []);
+
+  if (initializing) return null;
+
+
+
+    return (
+        
+        <View style={{flex: 1, backgroundColor: '#070A1E'}}>
+       <Header
+            barStyle="default"
+            centerComponent={
+                <VFlex>
+                <TopTitle>SIGN UP</TopTitle>
+                </VFlex>
+                }
+            centerContainerStyle={{}}
+            containerStyle={{ width: '100%', backgroundColor: 'none', borderBottomColor: "transparent"}}
+            leftComponent={   
+                <BackArrow onPress={() => navigation.goBack()}>
+                    <Icon name='angle-left' size={25} style={{color:"white"}}/>
+                </BackArrow>
+                }
+            placement="center"
+            rightComponent={{}}
+            rightContainerStyle={{}}
+            
+            statusBarProps={{}}
+    />
+    
+        <VFlex style={{paddingTop:'15%'}}>
+            <Image source={require("../../../assets/images/getstarted.png")} 
+                   style={{height:360, width:360, resizeMode:"contain"}}/>
+        </VFlex>
+        <ButtonView>
+            <ButtonGoogle onPress={() => onGoogleButtonPress()}>
+            <HFlex>
+                <Icon name='google' size={20} style={{color:"white", paddingRight:20}}/>
+                <Text style={{color:"white"}}>Sign Up with Google</Text>
+                </HFlex>
+                
+            </ButtonGoogle>
+            <ButtonFacebook onPress={() => navigation.navigate('Completion')}>
+            <HFlex>
+                <Icon name='facebook-f' size={20} style={{color:"white", paddingRight:20}}/>
+                <Text style={{color:"white"}}>Sign Up with Facebook</Text>
+                </HFlex>
+            </ButtonFacebook>
+        </ButtonView>
+        </View>
+    )
+}
