6e02507006e6b7f8fa7309aa78160f2f3a85c49f
Harman <hrrsandhu6@gmail.com>
2021-05-18 10:55:28 -0700

    load models

--- app/App.js
+++ app/App.js
@@ -1,11 +1,14 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
 import {ApolloProvider} from "@apollo/client"
 import {apolloClient} from "apollo/apollo"
 import { AuthNavigator } from 'navigation/main/AuthNavigator';
 import { UserNavigator } from './src/navigation/user/UserNavigator';
-
+import { Alert } from 'react-native'
+import AppController from 'controllers/AppController'
+import { View } from 'react-native'
+import { Text } from 'react-native'
 
 
 // here
@@ -13,11 +16,37 @@ const Stack = createStackNavigator()
 
 // App component always stays here
 export default function App(){
+    const [isLoading, setIsLoading] = useState(true)
+
+    async function load(){
+        try{
+            await AppController.load()
+            setIsLoading(false)
+        }catch (error) {
+            Alert.alert("Something went wrong", error.message, [
+                {
+                    text: "Retry",
+                    onPress: () => load(),
+                },
+            ]);
+        }
+    }
+
+    useEffect(() => {
+        load()
+    }, [])
+
     return (
-        <ApolloProvider client={apolloClient}>
-                <AuthNavigator /> 
-        </ApolloProvider>
-    )
+        isLoading ? ( 
+             <View style={{flex: 1}}>
+                 <Text>loading</Text>
+             </View>
+             ) : ( 
+                  <ApolloProvider client={apolloClient}>
+                     <AuthNavigator /> 
+                 </ApolloProvider> 
+             )
+     )
 }
 
 
